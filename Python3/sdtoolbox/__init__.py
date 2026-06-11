cebf872542db43c84f4715bba8a5ac07843e1401
HarryZhou <2373256746@qq.com>
2026-06-11 18:48:30 +0800

    feat: add pip-installable packaging with pyproject.toml
    - Add pyproject.toml with setuptools build backend
    - Include LICENCE.txt in package distribution
    - Add __version__ attribute for dynamic versioning
    - Add .gitignore for build artifacts
    - Install via: pip install "sdtoolbox @ git+https://github.com/harryzhou2000/sdtoolbox.git#subdirectory=Python3"
    
    committed by opencode with model deepseek-v4-pro

--- Python3/sdtoolbox/__init__.py
+++ Python3/sdtoolbox/__init__.py
@@ -2,6 +2,8 @@
  Shock and Detonation Toolbox
  http://www.galcit.caltech.edu/EDL/PublicResources/SDT/
 '''
+__version__ = "2026.04"
+
 import sdtoolbox.postshock
 import sdtoolbox.reflections
 import sdtoolbox.thermo
